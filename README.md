b04bfb277bcc0d17772a3b3486976d4f95578cc8
vrondakis <manolisvrondakis@gmail.com>
2014-09-07 17:27:36 +0100

    Added DB Module
    +Updated Readme

--- README.md
+++ README.md
@@ -1,4 +1,37 @@
-katjs
+KatJS
 =====
 
-A Lightweight NodeJS Library
+A lightweight NodeJS Library. 
+
+
+Usage
+=====
+
+Put it in your node_modules folder and add initialize it in your project.
+
+```js
+var KAT = require('katjs');
+KAT.Init();
+```
+
+A typical set-up might look something like this
+
+```
+var KAT = require('katjs');
+
+KAT.DB.Connect('host','username','password','database');
+
+// Query a single value
+KAT.DB.QueryValue("SELECT count(*) FROM users");
+
+// Query a single row
+KAT.DB.QueryRow("SELECT id,name FROM users WHERE id = ?", [1]);
+
+//Query a whole table
+KAT.DB.Query("SELECT id FROM users WHERE name = ?, shoesize = ?",['Manolis', 11]);
+
+
+
+
+```
+
---
+++ katjs/database.js
@@ -0,0 +1,180 @@
+/*
+	Database
+	.Connect(host, username, password, database)
+
+	Query a single value:
+	.QueryValue("SELECT count(*) FROM users");
+	
+	Query a single row:
+	.QueryRow("SELECT id,name,shoesize FROM users WHERE id = ?", ['1']);
+
+	Query a table:
+	.Query("SELECT id,name,shoesize FROM users WHERE id = ?", ['1']);
+
+	Inserting:
+	.Insert('users', [{'name'=>'manolis','shoesize'=>11}, {'name'=>'James', 'shoesize'=>4}]);
+
+
+*/
+var mysql = require('mysql')
+var queries = 0;
+var connection;
+exports.Connect = function(ip,username,password,database){
+	connection = mysql.createConnection({
+		host:ip,
+		user:username,
+		password:password,
+		database:database
+	})
+
+	connection.connect(function(error){
+		if(error){
+			console.error('Error connecting to IP '+ip+'. Error: '+error.stack);
+			return;
+		}
+
+		connection.on("error", function (err) {
+			if (!err.fatal) {
+				return;
+			}
+
+			if (err.code !== "PROTOCOL_CONNECTION_LOST") {
+				throw err;
+			}
+
+			connection.connect(function(error){
+				if(error){
+					process.exit(1);
+				}
+			});
+
+		});
+
+		process.on('exit', function() {
+			if(connection){
+				connection.end();
+			}
+		});
+
+		console.log('Connected to MySQL Database');
+	});
+
+
+}
+
+function QueryInternal(str,array, callback){
+	console.log('Starting query: '+str);
+	queries++;
+	var curTime = (new Date).getTime();
+
+	connection.query(str, array, function(err,data){
+		if(err){
+			console.error('MySQL Error: '+err);
+		}
+		else{
+			if(callback){
+				callback(data);
+			}
+		}
+	});
+}
+
+function reset(arr) {
+	var val;
+	for(var key in arr){
+		val = arr[key];
+	}
+	this.php_js = this.php_js || {};
+	this.php_js.pointers = this.php_js.pointers || [];
+	var indexOf = function(value) {
+		for (var i = 0, length = this.length; i < length; i++) {
+			if (this[i] === value) {
+				return i;
+			}
+		}
+		return -1;
+	};
+	var pointers = this.php_js.pointers;
+	if (!pointers.indexOf) {
+		pointers.indexOf = indexOf;
+	}
+	if (pointers.indexOf(arr) === -1) {
+		pointers.push(arr, 0);
+	}
+	var arrpos = pointers.indexOf(arr);
+	if (Object.prototype.toString.call(arr) !== '[object Array]') {
+		for (var k in arr) {
+			if (pointers.indexOf(arr) === -1) {
+				pointers.push(arr, 0);
+			} else {
+				pointers[arrpos + 1] = 0;
+			}
+			return arr[k];
+		}
+		return false;
+	}
+
+	if (arr.length === 0) {
+		return false;
+	}
+	pointers[arrpos + 1] = 0;
+	return arr[pointers[arrpos + 1]];
+}
+
+
+
+exports.Query = function(str,array,callback){
+	QueryInternal(str,array, function(data){
+		if(callback){
+			callback(data);
+		}
+	});
+}
+
+exports.QueryRow = function(str,array,callback){
+	QueryInternal(str,array,function(data){
+		if(callback){
+			callback(reset(data))
+		}
+	})
+}
+
+exports.QueryValue = function(str,array,callback){
+	exports.QueryRow(str,array,function(data){
+		if(callback){
+			callback(reset(data))
+		}
+	})
+}
+
+exports.Insert = function(table,array,callback, insert){
+	insert = typeof insert !== 'undefined' ? insert : 'INSERT';
+	var keys = [];
+	var keys_val = [];
+	var values = [];
+
+	for(var i = 0; i<array.length; i++){
+		for(var key in array[i]){
+			keys.push(key);
+			values.push(array[i][key]);
+		}
+		keys_val.push('?');
+	}
+
+	var str = insert+" INTO "+table+" ("+keys.join(',')+") VALUES ("+keys_val.join(',')+")";
+
+
+	exports.Query(str, values, function(data){
+		if(callback){
+			callback(data.insertId);
+		}
+	});
+
+}
+
+exports.Replace = function(table,array, callback){
+	exports.Insert(table,array,callback,"REPLACE");
+}
+
+
+
