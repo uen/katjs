7914967387ddc84b5e4be15b77b38728a8dd3b8f
Manolis Vrondakis <manolisvrondakis@gmail.com>
2014-10-19 20:06:26 +0100

    Update index.js

--- katjs/index.js
+++ katjs/index.js
@@ -2,9 +2,10 @@ exports.cfg = {
   reporter:null
 }
 
-exports.Config = function(k,v){
+exports.SetConfig = function(k,v){
   exports.cfg[k] = v;
 }
 
 exports.DB = require('./database');
 exports.SOCKET = require('./socket');
+
